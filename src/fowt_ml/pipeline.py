2b6ff5dc5e9e511cafa31c6e16a56a7ba60e21cc
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-01-16 16:15:03 +0100

    fix linter errors

--- src/fowt_ml/pipeline.py
+++ src/fowt_ml/pipeline.py
@@ -1,5 +1,4 @@
 import logging
-from pathlib import Path
 from typing import Any
 import pandas as pd
 from fowt_ml.config import read_yaml
@@ -40,9 +39,8 @@ class Pipeline:
             Experiment object or similar.
 
         """
-        ml_setup = self.config["ml_setup"]
-
         # TODO:
+        # ml_setup = self.config["ml_setup"]
         # train_test_split
         # setup or init the experiment
         # self.exp = ...
@@ -62,20 +60,19 @@ class Pipeline:
 
         ml_setup = self.config["ml_setup"]
         if ml_setup.get("save_grid_scores", False):
-            session_setup = self.config["session_setup"]
-            file_name = Path(session_setup["work_dir"]) / "grid_scores.csv"
             raise NotImplementedError("Saving grid score is not implemented yet.")
             # TODO: fix it
+            # session_setup = self.config["session_setup"]
+            # file_name = Path(session_setup["work_dir"]) / "grid_scores.csv"
             # self.grid_scores = experiment.pull()
             # self.grid_scores.to_csv(file_name, index=False)
             # logger.info(f"Grid scores saved to {file_name}.")
 
         if ml_setup.get("save_best_model", False):
-            session_setup = self.config["session_setup"]
-            file_name = Path(session_setup["work_dir"]) / "best_model.onnx"
             raise NotImplementedError("Saving best model is not implemented yet.")
             # TODO: fix it
+            # session_setup = self.config["session_setup"]
+            # file_name = Path(session_setup["work_dir"]) / "best_model.onnx"
             # save_sklearn_to_onnx(file_name)
             # logger.info(f"Best model saved to {file_name}.")
         raise NotImplementedError("Compare models is not implemented yet.")
-        return best_model
