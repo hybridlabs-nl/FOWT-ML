1f814068b44f3e92cca92901298a3664da4750ea
SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>
2025-01-31 17:09:53 +0100

    Add linear regression  (#15), fix sonar cloud workflow
    * add example using mlflow
    
    * add adraft module for linear models
    
    * update the nb
    
    * refactor liner modules
    
    * add tests for linear modules
    
    * add mlflow to pyproject
    
    * update nb
    
    * fix linter errors, add more tests
    
    * refactor imports
    
    * fix linter errors
    
    * add pytest coverage to sonar workflow
    
    * add debug flag to sonar workflow
    
    * add verbose to sonar workflow
    
    * refactor coverage command in sonar workflow
    
    * add debug command to sonar workflow
    
    * refactor sonar workflow for debugging
    
    * install in editable mode in sonar workflow
    
    * add kwargs to linear_models
    
    * remove import Dict
    
    * fix the path to the data in nb
    
    * apply review comments, and update tests and nb
    
    * remove self.model

---
+++ src/fowt_ml/linear_models.py
@@ -0,0 +1,65 @@
+"""Module to handle linear models."""
+
+from collections.abc import Iterable
+from typing import Any
+import sklearn.linear_model as lm
+import sklearn.metrics as sm
+from numpy.typing import ArrayLike
+from sklearn.base import BaseEstimator
+
+
+class LinearModels:
+    """Class to handle linear models and metrics for comparison."""
+
+    ESTIMATOR_NAMES = {
+        "LinearRegression": lm.LinearRegression,
+        "RidgeRegression": lm.Ridge,
+        "LassoRegression": lm.Lasso,
+        "ElasticNetRegression": lm.ElasticNet,
+        "LeastAngleRegression": lm.Lars,
+    }
+
+    def __init__(
+        self, estimator: str | BaseEstimator, **kwargs: dict[str, Any]
+    ) -> None:
+        """Initialize the class with the estimator."""
+        if isinstance(estimator, str):
+            if not self.ESTIMATOR_NAMES.get(estimator):
+                msg = (
+                    f"estimator {estimator} not supported. "
+                    f"Choose one of {list(self.ESTIMATOR_NAMES.keys())}"
+                    f"or pass a Estimator instance."
+                )
+                raise ValueError(msg)
+            self.estimator = self.ESTIMATOR_NAMES.get(estimator)(**kwargs)
+        elif isinstance(estimator, BaseEstimator):
+            self.estimator = estimator.set_params(**kwargs)
+        else:
+            raise ValueError("model must be a string or a Estimator instance.")
+
+    def calculate_score(
+        self,
+        x_train: ArrayLike,
+        x_test: ArrayLike,
+        y_train: ArrayLike,
+        y_test: ArrayLike,
+        scoring: str | Iterable = None,
+    ) -> float:
+        """Calculate the score for the model using test data.
+
+        First it fits the model with the training data, then predicts the test
+        data
+
+        Args:
+            x_train (ArrayLike): training data for features
+            x_test (ArrayLike): test data for features
+            y_train (ArrayLike): training data for targets
+            y_test (ArrayLike): test data for targets
+            scoring (Union[str, scoring]): the scoring to calculate
+
+        Returns:
+            float: the scoring value
+        """
+        self.estimator.fit(x_train, y_train)
+        scorer = sm.check_scoring(self.estimator, scoring=scoring)
+        return scorer(self.estimator, x_test, y_test)
---
+++ tests/test_linear_models.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pytest
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import Lars
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from fowt_ml.linear_models import LinearModels
+
+
+@pytest.fixture
+def simple_dataset():
+    """Create a simple dataset for testing."""
+    x_train = [[1, 2], [3, 4], [5, 6]]
+    x_test = [[7, 8], [9, 10], [11, 12]]
+    y_train = [1, 2, 3]
+    y_test = [4, 5, 6]
+    return x_train, x_test, y_train, y_test
+
+
+class TestLinearModel:
+    def test_init_estimator_lr(self):
+        model = LinearModels("LinearRegression")
+        assert isinstance(model.estimator, LinearRegression)
+
+    def test_init_estimator_ridge(self):
+        model = LinearModels("RidgeRegression")
+        assert isinstance(model.estimator, Ridge)
+
+    def test_init_estimator_lasso(self):
+        model = LinearModels("LassoRegression")
+        assert isinstance(model.estimator, Lasso)
+
+    def test_init_estimator_elasticnet(self):
+        model = LinearModels("ElasticNetRegression")
+        assert isinstance(model.estimator, ElasticNet)
+
+    def test_init_estimator_lars(self):
+        model = LinearModels("LeastAngleRegression")
+        assert isinstance(model.estimator, Lars)
+
+    def test_init_with_estimator_instance(self):
+        model = LinearModels(Lars())
+        assert isinstance(model.estimator, BaseEstimator)
+
+    def test_init_with_invalid_name(self):
+        with pytest.raises(ValueError) as e:
+            LinearModels("LinearRegressionInvalid")
+        assert "estimator LinearRegressionInvalid not supported" in str(e.value)
+
+    def test_init_with_invalid_instance(self):
+        with pytest.raises(ValueError) as e:
+            LinearModels(Lars)
+        assert "model must be a string or a Estimator instance" in str(e.value)
+
+    def test_init_estimator_kwargs(self):
+        model = LinearModels("LinearRegression", fit_intercept=False)
+        assert model.estimator.fit_intercept is False
+
+    def test_calculate_score_rmse(self, simple_dataset):
+        x_train, x_test, y_train, y_test = simple_dataset
+        model = LinearModels("LinearRegression")
+        results = model.calculate_score(
+            x_train, x_test, y_train, y_test, "neg_mean_squared_log_error"
+        )
+        np.testing.assert_almost_equal(results, 0.0, decimal=3)
+
+    def test_calculate_score_more(self, simple_dataset):
+        x_train, x_test, y_train, y_test = simple_dataset
+        model = LinearModels("LinearRegression")
+        results = model.calculate_score(
+            x_train,
+            x_test,
+            y_train,
+            y_test,
+            ["neg_mean_squared_error", "neg_mean_squared_log_error"],
+        )
+        np.testing.assert_almost_equal(
+            results["neg_mean_squared_error"], -0.0, decimal=3
+        )
+        np.testing.assert_almost_equal(
+            results["neg_mean_squared_log_error"], -0.0, decimal=3
+        )
