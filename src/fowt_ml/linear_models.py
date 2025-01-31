95405ae7aad3de4a63a8a11664f50d73e77a9702
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-01-31 16:49:28 +0100

    apply review comments, and update tests and nb

--- src/fowt_ml/linear_models.py
+++ src/fowt_ml/linear_models.py
@@ -1,43 +1,27 @@
 """Module to handle linear models."""
 
-from dataclasses import dataclass
+from collections.abc import Iterable
+from typing import Any
 import sklearn.linear_model as lm
 import sklearn.metrics as sm
 from numpy.typing import ArrayLike
-
-
-@dataclass
-class Estimator:
-    name: str
-    func: callable
-    reference: str = "sklearn"
-
-
-@dataclass
-class Metric:
-    name: str
-    func: callable
-    reference: str = "sklearn"
+from sklearn.base import BaseEstimator
 
 
 class LinearModels:
     """Class to handle linear models and metrics for comparison."""
 
     ESTIMATOR_NAMES = {
-        "LinearRegression": lm.LinearRegression(),
-        "RidgeRegression": lm.Ridge(),
-        "LassoRegression": lm.Lasso(),
-        "ElasticNetRegression": lm.ElasticNet(),
-        "LeastAngleRegression": lm.Lars(),
-    }
-    METRICS_NAMES = {
-        "root_mean_squared_error": sm.root_mean_squared_error,
-        "mean_squared_error": sm.mean_squared_error,
-        "r2_score": sm.r2_score,
-        "mean_absolute_error": sm.mean_absolute_error,
+        "LinearRegression": lm.LinearRegression,
+        "RidgeRegression": lm.Ridge,
+        "LassoRegression": lm.Lasso,
+        "ElasticNetRegression": lm.ElasticNet,
+        "LeastAngleRegression": lm.Lars,
     }
 
-    def __init__(self, estimator: str | Estimator, kwargs: dict = None) -> None:
+    def __init__(
+        self, estimator: str | BaseEstimator, **kwargs: dict[str, Any]
+    ) -> None:
         """Initialize the class with the estimator."""
         if isinstance(estimator, str):
             if not self.ESTIMATOR_NAMES.get(estimator):
@@ -47,27 +31,21 @@ class LinearModels:
                     f"or pass a Estimator instance."
                 )
                 raise ValueError(msg)
-            self.estimator = Estimator(estimator, self.ESTIMATOR_NAMES.get(estimator))
-        elif isinstance(estimator, Estimator):
-            self.estimator = estimator
-            # TODO: validate if model function is a callable and valid
+            self.estimator = self.ESTIMATOR_NAMES.get(estimator)(**kwargs)
+        elif isinstance(estimator, BaseEstimator):
+            self.estimator = estimator.set_params(**kwargs)
         else:
             raise ValueError("model must be a string or a Estimator instance.")
 
-        # fill the estimator with the kwargs
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self.estimator.func, key, value)
-
-    def calculate_metric(
+    def calculate_score(
         self,
         x_train: ArrayLike,
         x_test: ArrayLike,
         y_train: ArrayLike,
         y_test: ArrayLike,
-        metric: str | Metric,
+        scoring: str | Iterable = None,
     ) -> float:
-        """Calculate the metric for the model using test data.
+        """Calculate the score for the model using test data.
 
         First it fits the model with the training data, then predicts the test
         data
@@ -77,24 +55,11 @@ class LinearModels:
             x_test (ArrayLike): test data for features
             y_train (ArrayLike): training data for targets
             y_test (ArrayLike): test data for targets
-            metric (Union[str, Metric]): the metric to calculate
+            scoring (Union[str, scoring]): the scoring to calculate
 
         Returns:
-            float: the metric value
+            float: the scoring value
         """
-        if isinstance(metric, str):
-            if not self.METRICS_NAMES.get(metric):
-                msg = (
-                    f"metric {metric} not supported. "
-                    f"Choose one of {list(self.METRICS_NAMES.keys())}"
-                    f"or pass a Metric instance."
-                )
-                raise ValueError(msg)
-            self.metric = Metric(metric, self.METRICS_NAMES.get(metric))
-        elif not isinstance(metric, Metric):
-            raise ValueError("metric must be a string or a Metric instance.")
-
-        # TODO: check other arguments of fit, predict and metric functions
-        self.model = self.estimator.func.fit(x_train, y_train)
-        self.y_pred = self.model.predict(x_test)
-        return self.metric.func(y_test, self.y_pred)
+        self.model = self.estimator.fit(x_train, y_train)
+        scorer = sm.check_scoring(self.estimator, scoring=scoring)
+        return scorer(self.estimator, x_test, y_test)
--- tests/test_linear_models.py
+++ tests/test_linear_models.py
@@ -1,43 +1,12 @@
 import numpy as np
 import pytest
+from sklearn.base import BaseEstimator
 from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import Lars
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-from sklearn.metrics import root_mean_squared_error
-from fowt_ml.linear_models import Estimator
 from fowt_ml.linear_models import LinearModels
-from fowt_ml.linear_models import Metric
-
-
-class TestEstimator:
-    def test_init_estimator(self):
-        estimator = Estimator("LinearRegression", LinearRegression())
-        assert estimator.name == "LinearRegression"
-        assert isinstance(estimator.func, LinearRegression)
-
-    def test_init_estimator_with_reference(self):
-        estimator = Estimator("LinearRegression", LinearRegression(), "sklearn")
-        assert estimator.name == "LinearRegression"
-        assert isinstance(estimator.func, LinearRegression)
-        assert estimator.reference == "sklearn"
-
-
-class TestMetric:
-    def test_init_metric(self):
-        metric = Metric("mean_squared_error", mean_squared_error)
-        assert metric.name == "mean_squared_error"
-        assert metric.func == mean_squared_error
-
-    def test_init_metric_with_reference(self):
-        metric = Metric("mean_squared_error", mean_squared_error, "sklearn")
-        assert metric.name == "mean_squared_error"
-        assert metric.func == mean_squared_error
-        assert metric.reference == "sklearn"
 
 
 @pytest.fixture
@@ -53,34 +22,27 @@ def simple_dataset():
 class TestLinearModel:
     def test_init_estimator_lr(self):
         model = LinearModels("LinearRegression")
-        assert model.estimator.name == "LinearRegression"
-        assert isinstance(model.estimator.func, LinearRegression)
+        assert isinstance(model.estimator, LinearRegression)
 
     def test_init_estimator_ridge(self):
         model = LinearModels("RidgeRegression")
-        assert model.estimator.name == "RidgeRegression"
-        assert isinstance(model.estimator.func, Ridge)
+        assert isinstance(model.estimator, Ridge)
 
     def test_init_estimator_lasso(self):
         model = LinearModels("LassoRegression")
-        assert model.estimator.name == "LassoRegression"
-        assert isinstance(model.estimator.func, Lasso)
+        assert isinstance(model.estimator, Lasso)
 
     def test_init_estimator_elasticnet(self):
         model = LinearModels("ElasticNetRegression")
-        assert model.estimator.name == "ElasticNetRegression"
-        assert isinstance(model.estimator.func, ElasticNet)
+        assert isinstance(model.estimator, ElasticNet)
 
     def test_init_estimator_lars(self):
         model = LinearModels("LeastAngleRegression")
-        assert model.estimator.name == "LeastAngleRegression"
-        assert isinstance(model.estimator.func, Lars)
+        assert isinstance(model.estimator, Lars)
 
     def test_init_with_estimator_instance(self):
-        estimator = Estimator("LinearRegression", LinearRegression())
-        model = LinearModels(estimator)
-        assert model.estimator.name == "LinearRegression"
-        assert isinstance(model.estimator.func, LinearRegression)
+        model = LinearModels(Lars())
+        assert isinstance(model.estimator, BaseEstimator)
 
     def test_init_with_invalid_name(self):
         with pytest.raises(ValueError) as e:
@@ -89,54 +51,34 @@ class TestLinearModel:
 
     def test_init_with_invalid_instance(self):
         with pytest.raises(ValueError) as e:
-            LinearModels(LinearRegression())
+            LinearModels(Lars)
         assert "model must be a string or a Estimator instance" in str(e.value)
 
     def test_init_estimator_kwargs(self):
-        model = LinearModels("LinearRegression", {"fit_intercept": False})
-        assert model.estimator.func.fit_intercept is False
+        model = LinearModels("LinearRegression", fit_intercept=False)
+        assert model.estimator.fit_intercept is False
 
-    def test_calculate_metric_rmse(self, simple_dataset):
+    def test_calculate_score_rmse(self, simple_dataset):
         x_train, x_test, y_train, y_test = simple_dataset
         model = LinearModels("LinearRegression")
-        results = model.calculate_metric(
-            x_train, x_test, y_train, y_test, "root_mean_squared_error"
+        results = model.calculate_score(
+            x_train, x_test, y_train, y_test, "neg_mean_squared_log_error"
         )
-        assert model.metric.name == "root_mean_squared_error"
-        assert model.metric.func == root_mean_squared_error
         np.testing.assert_almost_equal(results, 0.0, decimal=3)
 
-    def test_calculate_metric_mse(self, simple_dataset):
+    def test_calculate_score_more(self, simple_dataset):
         x_train, x_test, y_train, y_test = simple_dataset
         model = LinearModels("LinearRegression")
-        results = model.calculate_metric(
-            x_train, x_test, y_train, y_test, "mean_squared_error"
+        results = model.calculate_score(
+            x_train,
+            x_test,
+            y_train,
+            y_test,
+            ["neg_mean_squared_error", "neg_mean_squared_log_error"],
         )
-        assert model.metric.name == "mean_squared_error"
-        assert model.metric.func == mean_squared_error
-        np.testing.assert_almost_equal(results, 0.0, decimal=3)
-
-    def test_calculate_metric_r2(self, simple_dataset):
-        x_train, x_test, y_train, y_test = simple_dataset
-        model = LinearModels("LinearRegression")
-        results = model.calculate_metric(x_train, x_test, y_train, y_test, "r2_score")
-        assert model.metric.name == "r2_score"
-        assert model.metric.func == r2_score
-        np.testing.assert_almost_equal(results, 1.0, decimal=3)
-
-    def test_calculate_metric_mae(self, simple_dataset):
-        x_train, x_test, y_train, y_test = simple_dataset
-        model = LinearModels("LinearRegression")
-        results = model.calculate_metric(
-            x_train, x_test, y_train, y_test, "mean_absolute_error"
+        np.testing.assert_almost_equal(
+            results["neg_mean_squared_error"], -0.0, decimal=3
         )
-        assert model.metric.name == "mean_absolute_error"
-        assert model.metric.func == mean_absolute_error
-        np.testing.assert_almost_equal(results, 0.0, decimal=3)
-
-    def test_calculate_metric_with_invalid_metric(self, simple_dataset):
-        x_train, x_test, y_train, y_test = simple_dataset
-        model = LinearModels("LinearRegression")
-        with pytest.raises(ValueError) as e:
-            model.calculate_metric(x_train, x_test, y_train, y_test, "r2_score_invalid")
-        assert "metric r2_score_invalid not supported" in str(e.value)
+        np.testing.assert_almost_equal(
+            results["neg_mean_squared_log_error"], -0.0, decimal=3
+        )
