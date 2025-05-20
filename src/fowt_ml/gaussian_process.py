f614e1a1358857f54a880513deff3bd43de960ff
SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>
2025-05-20 16:12:50 +0200

    add sparse guassian process module (#22)
    * add guassian module
    
    * fix linter errors
    
    * add gpytorch to dependencies
    
    * fix typo in file name
    
    * fix a typo
    
    * add a nb for gp
    
    * add a test for gaussian_process
    
    * fix format
    
    * add tests, fix the classes
    
    * replace print with logger
    
    * fix a bug in nb of linear models
    
    * add more tests
    
    * set seed in the module
    
    * fix gaussian nb
    
    * fix linter errors
    
    * set torch seed in test
    
    * fix type of array in get_tensorlike
    
    * fix linter on type
    
    * add tags, fix dtype
    
    * fix linters
    
    * fix test
    
    * rename mean/covar_module to mean/covar
    
    * make device global
    
    * rename get_tensorlike to to_tensor
    
    * fix ruff formatter

---
+++ src/fowt_ml/gaussian_process.py
@@ -0,0 +1,287 @@
+"""Module for sparse Gaussian process for multi-output regeression problem."""
+
+from collections.abc import Iterable
+from logging import Logger
+from typing import Any
+import gpytorch
+import pandas as pd
+import torch
+from numpy.typing import ArrayLike
+from sklearn.base import BaseEstimator
+from sklearn.base import RegressorMixin
+from sklearn.metrics import check_scoring
+from sklearn.metrics import r2_score
+from sklearn.utils.validation import check_array
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_X_y
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+
+logger = Logger(__name__)
+
+# Set the random seed for reproducibility
+torch.manual_seed(42)
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class MultitaskGPModelApproximate(gpytorch.models.ApproximateGP):
+    """Multitask GP model with approximate inference.
+
+    This module models similarities/correlation in the outputs simultaneously. Each
+    output dimension (task) is the linear combination of some latent function. Base
+    on example
+    https://docs.gpytorch.ai/en/stable/examples/04_Variational_and_Approximate_GPs/SVGP_Multitask_GP_Regression.html#Types-of-Variational-Multitask-Models
+    """
+
+    def __init__(self, inducing_points, num_latents, num_tasks):
+        # convert inducing points to tensor
+        inducing_points = _to_tensor(inducing_points)
+
+        # Variational distribution + strategy: posterior for latent GPs
+        # CholeskyVariationalDistribution: modeling a full covariance (not
+        # diagonal), so it can capture dependencies between inducing points
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
+            num_inducing_points=inducing_points.size(0),
+            batch_shape=torch.Size([num_latents]),
+        )
+
+        # Check inducing points shape before passing to VariationalStrategy
+        inducing_points = _check_inducing_points(inducing_points, num_latents)
+
+        # model correlations across tasks (or outputs)
+        variational_strategy = gpytorch.variational.LMCVariationalStrategy(
+            gpytorch.variational.VariationalStrategy(
+                self,
+                inducing_points,
+                variational_distribution,
+                learn_inducing_locations=True,
+            ),
+            num_tasks=num_tasks,
+            num_latents=num_latents,
+            latent_dim=-1,
+        )
+        super().__init__(variational_strategy)
+
+        # covariance module: kernel: Prior information about latents
+        self.covar = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_latents])),
+            batch_shape=torch.Size([num_latents]),
+        )
+        # Mean module
+        self.mean = gpytorch.means.ConstantMean(
+            batch_shape=torch.Size([num_latents]),
+        )
+
+        self.likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(
+            num_tasks=num_tasks
+        ).to(DEVICE)
+
+    def forward(self, x):
+        """Forward pass of the model."""
+        mean_x = self.mean(x)
+        covar_x = self.covar(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+
+class SklearnGPRegressor(RegressorMixin, BaseEstimator):
+    """Sklearn Wrapper for MultitaskGPModelApproximate."""
+
+    def __init__(
+        self,
+        num_inducing,
+        num_latents,
+        num_epochs=10,
+        batch_size=1024,
+        learning_rate=0.01,
+    ):
+        self.num_inducing = num_inducing
+        self.num_latents = num_latents
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+
+        # sklearn multioutput regressor expects float64
+        # see check_multioutput_regressor
+        torch.set_default_dtype(torch.float64)
+
+    def __sklearn_tags__(self):
+        tags = super().__sklearn_tags__()
+        tags.target_tags.two_d_labels = True
+        tags.target_tags.multi_output = True
+        tags.non_deterministic = True
+        tags.requires_fit = True
+        tags.input_tags.three_d_array = True
+        return tags
+
+    def fit(self, x_train: ArrayLike, y_train: ArrayLike) -> "SklearnGPRegressor":
+        """Fit the model to the training data."""
+        # Check that X and y have correct shape
+        x_train, y_train = check_X_y(x_train, y_train, multi_output=True)
+
+        x_train = _to_tensor(x_train)
+        y_train = _to_tensor(y_train)
+
+        # add some sklearn variables
+        self.X_ = x_train
+        self.y_ = y_train
+        self.n_features_in_ = x_train.shape[1]
+
+        # initialize model
+        if y_train.ndim == 1:
+            y_train = y_train.unsqueeze(1)
+
+        inducing_points = x_train[torch.randperm(x_train.size(0))[: self.num_inducing]]
+
+        self.model_ = MultitaskGPModelApproximate(
+            inducing_points=inducing_points,
+            num_latents=self.num_latents,
+            num_tasks=y_train.size(1),
+        ).to(DEVICE)
+
+        self.likelihood_ = self.model_.likelihood
+
+        # Train the model
+        self.model_.train()
+        self.likelihood_.train()
+
+        optimizer = torch.optim.Adam(self.model_.parameters(), lr=self.learning_rate)
+        # marginal log likelihood (mll)
+        mll = gpytorch.mlls.VariationalELBO(
+            self.likelihood_, self.model_, num_data=x_train.size(0)
+        )
+
+        # TODO optimize the loops
+        for epoch in range(self.num_epochs):
+            total_loss = 0
+            if self.batch_size:  # Use batching if batch_size is set
+                batches = DataLoader(
+                    TensorDataset(x_train, y_train),
+                    batch_size=self.batch_size,
+                )
+            else:  # Treat entire dataset as one batch
+                batches = [(x_train, y_train)]
+            for x_batch, y_batch in batches:
+                optimizer.zero_grad()
+                output = self.model_(x_batch)
+                loss = -mll(output, y_batch)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
+
+            # normalize the loss per data point and output dimension because it
+            # gives a better idea of the loss in log
+            ave_loss = total_loss / (x_train.size(0) * y_train.size(1))
+            logger.info(f"Epoch {epoch + 1}/{self.num_epochs} - Loss: {ave_loss:.3f}")
+
+        self.is_fitted_ = True
+        return self
+
+    def predict(self, x_array: ArrayLike) -> ArrayLike:
+        """Make predictions using the trained model."""
+        # Check if the model has been fitted
+        check_is_fitted(self, ["is_fitted_", "model_", "likelihood_"])
+
+        # Check that X has correct shape
+        x_array = check_array(x_array)
+
+        # Check number of features
+        if x_array.shape[1] != self.n_features_in_:
+            raise ValueError(
+                f"Expected {self.n_features_in_} features, "
+                f"but got {x_array.shape[1]} features."
+            )
+        x_array = _to_tensor(x_array)
+
+        self.model_.eval()
+        self.likelihood_.eval()
+
+        all_preds = []
+        with torch.no_grad():
+            if self.batch_size:  # Use batching if batch_size is set
+                batches = DataLoader(
+                    TensorDataset(x_array),
+                    batch_size=self.batch_size,
+                )
+            else:  # Treat entire dataset as one batch
+                batches = [(x_array,)]
+            for (x_batch,) in batches:
+                predictions = self.likelihood_(self.model_(x_batch))
+                all_preds.append(predictions.mean.cpu())
+
+        return torch.cat(all_preds, dim=0).numpy()
+
+    def score(self, x, y):
+        """Return the R^2 score of the prediction."""
+        y_pred = self.predict(x)
+        return r2_score(y, y_pred)
+
+
+class SparseGaussianModel:
+    """Class to handle sparse Gaussian process regression."""
+
+    ESTIMATOR_NAMES = {
+        "SklearnGPRegressor": SklearnGPRegressor,
+    }
+
+    def __init__(
+        self, estimator: str | BaseEstimator, **kwargs: dict[str, Any]
+    ) -> None:
+        if isinstance(estimator, str):
+            if estimator not in self.ESTIMATOR_NAMES:
+                raise ValueError(f"Available estimators: {self.ESTIMATOR_NAMES.keys()}")
+            self.estimator = self.ESTIMATOR_NAMES[estimator](**kwargs)
+        else:
+            self.estimator = estimator.set_params(**kwargs)
+
+    def calculate_score(
+        self,
+        x_train: ArrayLike,
+        x_test: ArrayLike,
+        y_train: ArrayLike,
+        y_test: ArrayLike,
+        scoring: str | Iterable | None = None,
+    ) -> float | ArrayLike:
+        """Fit and calculate a score.
+
+        In multi-output regression, by default, 'uniform_average' is used,
+        which specifies a uniformly weighted mean over outputs. see
+        https://scikit-learn.org/stable/modules/model_evaluation.html#regression-metrics
+
+        For scoring paramers overview:
+        https://scikit-learn.org/stable/modules/model_evaluation.html#string-name-scorers
+        """  # noqa: E501
+        self.estimator.fit(x_train, y_train)
+        scorer = check_scoring(self.estimator, scoring=scoring)
+        return scorer(self.estimator, x_test, y_test)
+
+
+def _to_tensor(array: ArrayLike | pd.DataFrame) -> torch.Tensor:
+    """Convert numpy array to tensor."""
+    dtype = torch.get_default_dtype()
+    if isinstance(array, torch.Tensor):
+        return array.to(DEVICE)
+    elif isinstance(array, pd.DataFrame):
+        return torch.tensor(array.values, dtype=dtype).to(DEVICE)
+    elif isinstance(array, Iterable):
+        return torch.tensor(array, dtype=dtype).to(DEVICE)
+    else:
+        raise ValueError("Input must be ArrayLike or pd.DataFrame.")
+
+
+def _check_inducing_points(x: torch.Tensor, num_latents: int) -> torch.Tensor:
+    """Check inducing points has correct shape."""
+    if x.ndim == 2:
+        x = x.unsqueeze(0).repeat(num_latents, 1, 1).clone()
+    elif x.ndim == 3:
+        if x.shape[0] != num_latents:
+            raise ValueError(
+                f"Inducing points should have {num_latents} latent dimensions, "
+                f"but got {x.shape[0]}."
+            )
+    else:
+        raise ValueError(
+            "Inducing points should be 2D [N, D] or 3D [B, N, D] tensor. "
+            f"Got {x.ndim} dimensions."
+        )
+    return x
---
+++ tests/test_gaussian_process.py
@@ -0,0 +1,161 @@
+import math
+from functools import partial
+import numpy as np
+import pytest
+import torch
+from sklearn.utils.estimator_checks import parametrize_with_checks
+from fowt_ml.gaussian_process import MultitaskGPModelApproximate
+from fowt_ml.gaussian_process import SklearnGPRegressor
+from fowt_ml.gaussian_process import SparseGaussianModel
+
+# Set the random seed for reproducibility
+torch.manual_seed(42)
+
+
+@pytest.fixture
+def simple_dataset():
+    """Create a simple dataset for testing."""
+    x_train = torch.rand(100, 2)
+
+    a = x_train[:, 0] * (2 * math.pi)
+    b = x_train[:, 1] * (2 * math.pi)
+    y_train = torch.stack(
+        [
+            torch.sin(a) + torch.randn(100) * 0.2,
+            torch.cos(a) + torch.randn(100) * 0.2,
+            torch.sin(b) + 2 * torch.cos(b) + torch.randn(100) * 0.2,
+            -torch.cos(b) + torch.randn(100) * 0.2,
+        ],
+        -1,
+    )
+
+    x_test = torch.rand(50, 2)
+    a = x_test[:, 0] * (2 * math.pi)
+    b = x_test[:, 1] * (2 * math.pi)
+    y_test = torch.stack(
+        [
+            torch.sin(a) + torch.randn(50) * 0.2,
+            torch.cos(a) + torch.randn(50) * 0.2,
+            torch.sin(b) + 2 * torch.cos(b) + torch.randn(50) * 0.2,
+            -torch.cos(b) + torch.randn(50) * 0.2,
+        ],
+        -1,
+    )
+    return x_train, x_test, y_train, y_test
+
+
+class TestSparseGaussianModel:
+    def test_init_estimator_sklearn(self):
+        params = {
+            "num_inducing": 25,
+            "num_latents": 1,
+        }
+        model = SparseGaussianModel("SklearnGPRegressor", **params)
+        assert isinstance(model.estimator, SklearnGPRegressor)
+
+    def test_init_estimator_invalid(self):
+        with pytest.raises(ValueError) as e:
+            SparseGaussianModel("InvalidEstimator")
+        assert "Available estimators" in str(e.value)
+
+    def test_init_with_estimator_instance(self):
+        estimator = SklearnGPRegressor(25, 1)
+        model = SparseGaussianModel(estimator)
+        assert isinstance(model.estimator, SklearnGPRegressor)
+
+    def test_init_with_kwargs(self):
+        params = {
+            "num_inducing": 25,
+            "num_latents": 1,
+            "batch_size": 10,
+        }
+        model = SparseGaussianModel("SklearnGPRegressor", **params)
+        assert model.estimator.batch_size == 10
+
+    def test_calculate_score_rmse(self, simple_dataset):
+        x_train, x_test, y_train, y_test = simple_dataset
+
+        params = {
+            "num_inducing": 25,
+            "num_latents": 1,
+            "num_epochs": 1,
+            "batch_size": 10,
+        }
+        model = SparseGaussianModel("SklearnGPRegressor", **params)
+        results = model.calculate_score(
+            x_train, x_test, y_train, y_test, "neg_mean_squared_error"
+        )
+        np.testing.assert_almost_equal(results, -1.0103, decimal=3)
+
+
+def dummy_estimator():
+    return SklearnGPRegressor(100, 1, num_epochs=1, batch_size=10)
+
+
+@parametrize_with_checks([dummy_estimator()])
+def test_sklearn_compatibility(estimator, check):
+    # some of these checks may break due to GPyTorch-specific behavior and the
+    # wrapper implementation
+    exclude_checks = [
+        "check_regressors_train",
+        "check_fit_idempotent",
+        "check_fit_score_takes_y",
+        "check_supervised_y_2d",
+        "check_regressor_data_not_an_array",
+        "check_regressors_int",
+        "check_n_features_in_after_fitting",
+        "check_dtype_object",
+    ]
+    check_name = check.func.__name__ if isinstance(check, partial) else check.__name__
+    if check_name not in exclude_checks:
+        check(estimator)
+
+
+class TestSklearnGPRegressor:
+    def test_init(self):
+        model = SklearnGPRegressor(num_inducing=25, num_latents=1)
+        assert model.num_inducing == 25
+        assert model.num_latents == 1
+
+    def test_fit(self, simple_dataset):
+        x_train, _, y_train, _ = simple_dataset
+        model = SklearnGPRegressor(num_inducing=25, num_latents=1)
+        model.fit(x_train, y_train)
+        assert hasattr(model, "model_")
+        assert hasattr(model, "likelihood_")
+        assert hasattr(model, "n_features_in_")
+        assert model.is_fitted_
+
+    def test_predict(self, simple_dataset):
+        x_train, x_test, y_train, _ = simple_dataset
+        model = SklearnGPRegressor(num_inducing=25, num_latents=1)
+        model.fit(x_train, y_train)
+        predictions = model.predict(x_test)
+        assert predictions.shape == (50, 4)
+
+
+class TestMultitaskGPModelApproximate:
+    def test_init(self):
+        inducing_points = torch.rand(25, 2)
+        model = MultitaskGPModelApproximate(
+            inducing_points=inducing_points,
+            num_latents=1,
+            num_tasks=4,
+        )
+        assert hasattr(model, "variational_strategy")
+        assert hasattr(model, "covar")
+        assert hasattr(model, "mean")
+        assert hasattr(model, "likelihood")
+        assert model.variational_strategy.num_latents == 1
+        assert model.variational_strategy.num_tasks == 4
+        assert model.mean(inducing_points).shape == torch.Size([1, 25])
+        assert model.covar(inducing_points).shape == torch.Size([1, 25, 25])
+
+    def test_forward(self):
+        inducing_points = torch.rand(15, 2)
+        model = MultitaskGPModelApproximate(
+            inducing_points=inducing_points,
+            num_latents=1,
+            num_tasks=4,
+        )
+        assert model.forward(inducing_points).loc.shape == torch.Size([1, 15])
