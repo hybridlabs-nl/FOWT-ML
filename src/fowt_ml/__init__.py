b28604867f7ac485f9b8137c735c44388c22c0a4
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-09-12 11:21:34 +0200

    add classes to init of fowt_ml

--- src/fowt_ml/__init__.py
+++ src/fowt_ml/__init__.py
@@ -1,9 +1,22 @@
 """Documentation about fowt_ml."""
 
 import logging
+from fowt_ml.ensemble import EnsembleModel
+from fowt_ml.gaussian_process import SparseGaussianModel
+from fowt_ml.linear_models import LinearModels
+from fowt_ml.neural_network import NeuralNetwork
+from fowt_ml.xgboost import XGBoost
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Sarah Alidoost"
 __email__ = "f.alidoost@esciencecenter.nl"
 __version__ = "0.1.0"
+
+__all__ = [
+    "LinearModels",
+    "XGBoost",
+    "EnsembleModel",
+    "SparseGaussianModel",
+    "NeuralNetwork",
+]
