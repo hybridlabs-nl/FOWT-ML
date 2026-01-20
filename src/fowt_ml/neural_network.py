fix scaling in rnn models

src/fowt_ml/neural_network.py
@@ -9,7 +9,6 @@ from fowt_ml.base import BaseModel
 class GenericRNNModule(torch.nn.Module):
     def __init__(self, rnn_model, input_size, hidden_size, output_size, num_layers=1):
         super().__init__()
-        self.norm = torch.nn.LayerNorm(input_size)
         self.rnn = rnn_model(
             input_size=input_size,
             hidden_size=hidden_size,
@@ -25,10 +24,9 @@ class GenericRNNModule(torch.nn.Module):
         if x.dim() == 2:
             x = x.unsqueeze(1)  # add seq_len=1
 
-        x = self.norm(x)
         out, _ = self.rnn(x)  # (batch, seq_len, hidden)
         out = out[:, -1, :]  # take last time step (batch, hidden)
-        out_fc = self.fc(out)  # (batch, seq_len, output)
+        out_fc = self.fc(out)  # (batch, output)
         return out_fc
 
 